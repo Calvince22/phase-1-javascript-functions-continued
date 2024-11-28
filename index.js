2ea6fd552651f2c0ea53335c7d09616538e59014
Caleb Kiptoo <vincentcalebkip@gmail.com>
2024-11-28 12:50:05 +0300

    using scope functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// code your solution
+function saturdayFun(fun="roller-skate"){
+
+    return`This Saturday, I want to ${fun}!`
+}
+function mondayWork(work="go to the office"){
+        
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(wrapper){
+    return function(adjective){
+        return`You are ${wrapper}${adjective}${wrapper}!`
+    }
+}
+wrapAdjective("*")("stupid")
